ea3ee9dc7d8d9cd3a18433f9b071e91724bd71dd
Aarav Patel <aaravpatel14.in@gmail.com>
2026-09-11 12:23:34 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 echo "🧹 Cleaning up old builds..."
-rm -f *.class
+rm -f *.class *.jar
 
 echo "🔨 Compiling Game.java..."
 javac Game.java
 
 if [ $? -eq 0 ]; then
+    echo "📦 Packaging into java-snake-game.jar using manifest.mf..."
+    jar --create --file=java-snake-game.jar --manifest=manifest.mf *.class
+    
     echo "🚀 Launching Snake Game..."
-    java Game
+    java -jar java-snake-game.jar
 else
     echo "❌ Compilation failed."
 fi
